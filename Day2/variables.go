0754b9d69a25d4df185d471bd9f482844c2dcfa1
shahriar0999 <shahriar0999@proton.me>
2025-10-08 23:52:26 +0600

    variables in go

---
+++ Day2/variables.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main() {
+	var age int = 25 // Declering an integer variable
+	var name string = "Shahriar" // Declering a string variable
+	var isStudent bool = false // Declering a boolean variable
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Is Student:", isStudent)
+}
